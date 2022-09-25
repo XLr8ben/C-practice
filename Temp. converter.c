176e152b7e2de07e0e1a302b96efa9213746446a
XLr8ben <113667610+XLr8ben@users.noreply.github.com>
2022-09-25 21:32:33 +0530

    Add files via upload

---
+++ Temp. converter.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float c, far;
+    printf("value of C° is\n");
+    scanf("%f", &c);
+
+    
+    far = (c * 9/5) + 32;
+
+    printf("temp. in F° is %f", far);
+
+    return 0;
+}
+
