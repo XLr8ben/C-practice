f97aeefc8912597b198d08934d8671002638906f
XLr8ben <113667610+XLr8ben@users.noreply.github.com>
2022-09-28 09:40:34 +0530

    Add files via upload

---
+++ To_the_power.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("The value of 2^3 is %f\n", pow(2, 3));
+
+    return 0;
+}
