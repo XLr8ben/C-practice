d2856d62ed9594b6e4638935e6df141bc0f7969e
XLr8ben <113667610+XLr8ben@users.noreply.github.com>
2022-09-29 16:07:03 +0530

    Add files via upload

---
+++ If else_01.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("your age\n");
+    scanf("%d", &age);
+
+     if(age>=20)
+     
+     {printf("you r big");
+     
+     }
+
+     else
+     {
+      printf("you r small");
+    
+    
+    }
+     
+    
+
+    return 0;
+}
