967239d7113ed4943be32f3170c18aef7b97e0c2
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-10-01 14:25:13 +0530

    Update pass fail finder.c

--- pass fail finder.c
+++ pass fail finder.c
@@ -1,11 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
+//pass fail finder
 #include <stdio.h>
 
 int main()
