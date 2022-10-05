a80bb453dd76284b1474ca6c410ff4998763ebb6
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-10-05 21:57:36 +0530

    Update rating-teller-switch.c

--- rating-teller-switch.c
+++ rating-teller-switch.c
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//It is a program to give output depending on ur rating given
 
 #include <stdio.h>
 
