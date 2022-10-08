bdb01cd5d9e9f2d788dcc9fba37dde50eba0c779
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-10-08 21:43:27 +0530

    Update leap-year.c

--- leap-year.c
+++ leap-year.c
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//WAP to find a leap year
 
 #include <stdio.h>
 
