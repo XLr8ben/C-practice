37ed5b064d4854ae82302568a1d9a01378685759
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-10-23 20:24:03 +0530

    do-while-natural-no.

---
+++ do-while-natural-no..c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=1, n;
+    printf("enter no:");
+    scanf("%d", &n);
+    
+    do{
+        printf("natural no. are %d\n", a);
+        a++;
+    }
+    while(a<n);
+
+    return 0;
+}
