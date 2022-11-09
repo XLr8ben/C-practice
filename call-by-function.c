447ac5daf21e1f29ff32709c3370122763595f1e
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-11-09 19:12:50 +0530

    Add files via upload
    call-by-function

---
+++ call-by-function.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void goodmorning ();
+void goodafternoon ();
+void goodnight ();
+
+int
+main ()
+{
+  goodmorning ();
+  goodafternoon ();
+  goodnight ();
+
+
+  return 0;
+}
+
+void
+goodmorning ()
+{
+  printf ("morning bro\n");
+}
+
+void
+goodafternoon ()
+{
+  printf ("afternoon bro\n");
+
+}
+
+void
+goodnight ()
+{
+  printf ("night bro\n");
+}
