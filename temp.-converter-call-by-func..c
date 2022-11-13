9c065d322448bdd1d74e9c4005632b4443057a43
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-11-13 20:25:52 +0530

    Add files via upload
    temp.-converter-call-by-func.

---
+++ temp.-converter-call-by-func..c
@@ -0,0 +1,21 @@
+//Convert temperature from Celcius to Farhenheit(Call by function)
+
+#include <stdio.h>
+float temp (int c);
+
+int main ()
+{
+  int c;
+  printf ("Temp. in Celcius: ");
+  scanf ("%d", &c);
+
+  printf ("Temp in Fahrenheit: %.2f", temp (c));
+  return 0;
+}
+
+float temp (int c)
+{
+  //float far;
+  float far = (float) (c * 9 / 5) + 32;
+  return far;
+}
