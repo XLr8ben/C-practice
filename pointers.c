150e2025e14fa824a69134f196e05c62d3657da4
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2022-11-19 20:55:01 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i=5;
+    int *j = &i;
+    int **k= &j;
+    
+    printf("%d\n", *j);
+    printf("%d\n", i);
+    
+    printf("%u\n", &i);
+    printf("%u\n", *(&j));
+    printf("\n");
+    printf("%u\n", &k);
+    
+     int a=3;
+    int *ptr= &a;
+    printf("The address of a is %u\n", ptr);
+    printf("The value of a is %d\n", *ptr);
+    printf("The value of a is %d\n", ptr);
+    printf("The value of a is %d\n", &ptr);
+    printf("The value of a is %d\n", *(&ptr));
+    printf("The value of a is %d\n", a);
+    return 0;
+}
+}
+
+
