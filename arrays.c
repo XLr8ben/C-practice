8ea1520fb8b93b086fe08d4ee7b4c53e110fb666
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2023-01-26 16:52:47 +0530

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,21 @@
+//Arrays
+
+#include <stdio.h>
+
+int main ()
+{ //Noob way to store values without using arrays
+  int marks1, marks2, marks3, marks4;
+  marks1 = 12;
+  marks2 = 56;
+  marks3 = 76;
+  marks4 = 34;
+  
+  int marks[4];
+  marks[0] = 12;
+  marks[2] = 34;
+  marks[3] = 45;
+  marks[4] = 65;
+ 
+ return 0;
+}
+  
